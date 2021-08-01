0b6cc09ea3e7bb64e974b2fe2d5f4df360ddd935
Arman <naimur58co@gmail.com>
2021-08-01 17:42:50 +0600

    boolean type

---
+++ intro/bool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h> //this header file enable us to use bool type and true fals option
+int main()
+{
+    bool is_logged_in =true;
+    if (is_logged_in) {
+        printf("User Logged In\n");
+    } else {
+        printf("User Logged Out\n");
+    }
+} 
+//boolean also can be represented by intiger
