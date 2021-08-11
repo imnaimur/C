943398cd9cf3cbd5fc6916c2a765b3ef59bb6dc4
Arman <naimur58co@gmail.com>
2021-08-11 19:30:49 +0600

    if condition

---
+++ Condition/if_condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // int condition=1;
+    // if(condition){
+    //     printf("Execute this plan\n");
+    // }
+
+    int a=50,b=10;
+    int c=a>b;
+    if(c){                               //c=a>b
+        printf("A is greater than B\n");
+    }
+
+    
+    return 0;
+}
