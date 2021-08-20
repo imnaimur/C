a70e9df9965455781e2ae97441c7b6f377f7351d
Arman <naimur58co@gmail.com>
2021-08-21 04:28:59 +0600

    For loop

---
+++ Loop/forloop-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int i;
+
+    // for (i== 1; i<=5; i++){
+    //     printf("I'm Naimur\n",i);
+    // }
+
+
+    // printf("All odd number from 1-100\n");
+    // for (i ==0; i<=100; i++){
+    //     if(i%2 == 1){
+    //         printf("%d\n",i);
+    //     }
+    
+
+      printf("All Even number from 1-100\n");
+    for (i ==2; i<=100; i+=2){
+            printf("%d\n",i);
+    }
+    return 0;
+}
