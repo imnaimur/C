41221a4162a8c97d4bad7cacb6e84230da531d5e
Arman <naimur58co@gmail.com>
2021-08-31 20:26:12 +0600

    Do while loop transformation String

---
+++ task/do_while_uplo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    char name[30];
+    int i=0;
+    printf("Enter a string: ");
+    fgets(name, 30, stdin);
+    while(name[i]){
+        if(name[i] >= 'a' && name[i] <= 'z'){
+            name[i] = name[i] - 32;
+        }
+        i++;
+        
+    }
+    printf("Name %s\n",name);
+    return 0;
+}
